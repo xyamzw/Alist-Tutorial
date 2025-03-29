3d88b12c1a609422b7dd1e42b9ae5b1464c9ac94
xyamzw <2937150394@qq.com>
2025-03-30 03:58:42 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitePress-project/',
   title: "vitepress-alist",
   description: "一个alist教程",
-  base: '/vitePress-project/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
