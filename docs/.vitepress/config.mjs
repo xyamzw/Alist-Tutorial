94928dfe03bbef31d473888a9e2d91f474b5793a
xyamzw <2937150394@qq.com>
2025-03-30 04:07:30 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: 'Home', link: '/vitePress-project'},
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
